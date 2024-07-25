f2e5a15799d0b95c52ade20471fca99b2b8cf38b
MariaYasmeen <mariyayasmeen000@gmail.com>
2024-07-25 12:13:32 +0500

    final commit

--- mariayasmeen/src/Pages/Projects.jsx
+++ mariayasmeen/src/Pages/Projects.jsx
@@ -5,15 +5,15 @@ const Projects = () => {
         <>
  <Header />
    <div className="scrollable-content "> 
-                    <Link to="/typingtest">TypingTest<span>Commission 2024</span></Link> 
-                    <Link to="">CrypTrack<span> Commission 2024</span></Link>
-                    <Link to="">Home dashboard <span>Commission 2024</span></Link>  
-                    <Link to="">History book <span>Commission 2024</span></Link>  
-                    <Link to="">shop bazar <span>Commission 2024</span></Link>  
-                    <Link to="">PIANo boyi <span>Commission 2024</span></Link>  
-                    <Link to="">you know it <span>Commission 2024</span></Link>  
-                    <Link to="">NEWSChannel <span>Commission 2024</span></Link>  
-                    <Link to="">PLAin tracker <span> UI Project</span></Link>  
+                    <Link to="/typingtest">Typeathon <span>/ Commission 2024 </span></Link> 
+                    <Link to="">CrypTrack<span>/ Commission 2024</span></Link>
+                    <Link to="">Home dashboard <span>/ Commission 2024</span></Link>  
+                    <Link to="">History book <span>/ Commission 2024</span></Link>  
+                    <Link to="">shop bazar <span>/ Commission 2024</span></Link>  
+                    <Link to="">PIANo boyi <span>/ Commission 2024</span></Link>  
+                    <Link to="">you know it <span>/ Commission 2024</span></Link>  
+                    <Link to="">NEWSChannel <span>/ Commission 2024</span></Link>  
+                    <Link to="">PLAin tracker <span>/ UI Project</span></Link>  
                
      </div>
     
